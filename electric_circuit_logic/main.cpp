b6f1f1f0f531ab27423ce2e338b4b2174924dfa8
Joel Maxwell <jellyepicness@gmail.com>
2021-03-05 21:11:43 +1100

    Fixed AND,  OR Gates

--- electric_circuit_logic/main.cpp
+++ electric_circuit_logic/main.cpp
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+#include <stdint.h>
 
-#define DIODE_FORWARD_VOLTAGE 3.0f
+#define DIODE_FORWARD_VOLTAGE 1.0f
 #define VOLTAGE_DROP 0.95f //Drop 5%
 #define POWER_SUPPLY 5.0f
 #define HI_MIN 2.0f
@@ -14,43 +16,83 @@ float diode(float ANODE, float V_IN, float Forward_V);
 	//Logic Circuits
 float electrical_AND(float V1, float V2, float V_IN);
 float electrical_NAND(float V1, float V2, float V_IN);
-float electrical_OR(float V1, float V2, float VCC);
+float electrical_OR(float V1, float V2);
 float electrical_four_AND(float V1, float V2, float V3, float V4, float VCC);
 void electrical_buffer(float* V_IN);
 
+//Logical Expression Definitions
+float ABorC(float va, float vb, float vc);
 
 int main() {
 
-	float INPUT_A = 5.0f;
-	float INPUT_B = 5.0f;
-	float OUTPUT_Z = 0.0f;
+	printf("\t*** \t   TRUTH TABLE ( F = AB + C ) \t\    ***\n");
+	printf("\t||    A     |     B    |     C    |     F    ||\n");
 
-	OUTPUT_Z = electrical_AND(INPUT_A, INPUT_B, POWER_SUPPLY);
-	printf("( V1 = %f , V2 = %f ) -> AND_GATE -> ( VOUT = %f )\n", INPUT_A, INPUT_B, OUTPUT_Z);
-	float temp = OUTPUT_Z;
-	electrical_buffer(&OUTPUT_Z);
-	printf("( VOUT = %f ) -> BUFFER -> ( VOUT = %f )\n", temp, OUTPUT_Z);
+	for (uint8_t i = 0b00000000; i < 0b00001000; i++) {
 
-}
+		//Get Logic Inputs
+		float logicC = (float)(0b00000001 & i);
+		float logicB = (float)((0b00000010 & i) >> 1);
+		float logicA = (float)((0b00000100 & i) >> 2);
 
-//Takes in ANODE (Input), CATHODE (V_IN) -> Returns A voltage based on Inputs
-float diode(float ANODE, float V_IN, float Forward_V){
-	if (V_IN - ANODE > Forward_V) {
-		return 0.0f; //Current Flows Through Diode -> Voltage Goes to Zero
+		//Convert to Voltage
+		float va = logicA * POWER_SUPPLY;
+		float vb = logicB * POWER_SUPPLY;
+		float vc = logicC * POWER_SUPPLY;
+
+		//Run Logic Circuit
+		float vf = ABorC(va, vb, vc);
+		
+		//Convert Output to Logic 1 or 0 (Or Undefined)
+		float logicF = 0;
+		if (vf >= HI_MIN) {
+			logicF = 1;
+		}
+		else if (vf <= LOW_MAX) {
+			logicF = 0;
+		}
+		else {
+			logicF = NULL;
+		}
+
+		printf("\t|| %f | %f | %f | %f ||\n", logicA, logicB, logicC, logicF);
 	}
-	return V_IN;
+
+	printf("\t-----------------------------------------------\n");
+
+	return 0;
 }
 
-// Emulates a Voltage Drop
+// F = AB + C
+float ABorC(float va, float vb, float vc) {
+	float AND_OUTPUT = electrical_AND(va, vb, POWER_SUPPLY);
+	float OR_OUTPUT = electrical_OR(AND_OUTPUT, vc);
+	return OR_OUTPUT;
+}
+
+float diode(float ANODE, float CATHODE, float Forward_V){
+	if (CATHODE - ANODE > Forward_V) {
+		return CATHODE - Forward_V;
+	}
+	return 0.0f;
+}
+
 float resistor(float V_IN, float V_DROP) {
 	return V_IN * V_DROP;
 }
 
 
-float electrical_AND(float V1, float V2, float V_IN) {
+float electrical_AND(float v1, float v2, float V_IN) {
 	float circuit_voltage = resistor(V_IN, VOLTAGE_DROP);
-	circuit_voltage = diode(V1, circuit_voltage, DIODE_FORWARD_VOLTAGE);
-	circuit_voltage = diode(V2, circuit_voltage, DIODE_FORWARD_VOLTAGE);
+	circuit_voltage -= diode(v1, circuit_voltage, DIODE_FORWARD_VOLTAGE);
+	circuit_voltage -= diode(v2, circuit_voltage, DIODE_FORWARD_VOLTAGE);
+	return circuit_voltage;
+}
+
+float electrical_OR(float v1, float v2) {
+	float circuit_voltage = 0.0f;
+	circuit_voltage += diode(circuit_voltage, v1, DIODE_FORWARD_VOLTAGE);
+	circuit_voltage += diode(circuit_voltage, v2, DIODE_FORWARD_VOLTAGE);
 	return circuit_voltage;
 }
 

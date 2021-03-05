a7e1f3be733a31ac501c93d01d53ac7bf7f5615a
Joel Maxwell <jellyepicness@gmail.com>
2021-03-05 11:23:46 +1100

    Commit All

---
+++ electric_circuit_logic/main.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//Prototypes
+float electrical_AND(float V1, float V2, float VCC);
+float electrical_NAND(float V1, float V2, float VCC);
+float electrical_OR(float V1, float V2, float VCC);
+float electrical_four_AND(float V1, float V2, float V3, float V4, float VCC);
+
+
+
+
+
+int main() {
+
+
+	return 0;
+}
